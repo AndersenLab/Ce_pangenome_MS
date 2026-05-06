406a46bc8c57e201af8eb140197cccce89d2217b
lanceman999 <loconn13@jh.edu>
2026-05-06 10:55:52 -0400

    updated SV filtered TSV creation script

--- scripts/structural_variants/concatenate_filter_VCFs.sh
+++ scripts/structural_variants/concatenate_filter_VCFs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for file in *.vcf; do bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%FILTER\t%INFO/SVTYPE\t%INFO/SVLEN' $file | awk -v strain=${file%%.*} -v OFS='\t' '$7 >= 50 || $7 <= -50 {print $1,$2,$3,$4,$5,$6,$7,strain}'; done | grep -w "PASS" | grep -v -w "SNV"
+for file in *.vcf; do bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%FILTER\t%INFO/SVTYPE\t%INFO/SVLEN' $file | awk -v strain=${file%%.*} -v OFS='\t' '$7 >= 50 || $7 <= -50 {print $1,$2,$3,$4,$5,$6,$7,strain}'; done | grep -w "PASS" | grep -v -w "SNV" > ../../processed_data/structural_variants/141_over50_PASS_variants.tsv
