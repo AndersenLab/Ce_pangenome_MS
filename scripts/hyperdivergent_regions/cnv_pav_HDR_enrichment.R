5c2a15b3232aac95469ac30d0245e7be3b926e43
lanceman999 <loconn13@jh.edu>
2026-06-27 16:54:17 -0400

    updated script to fix the CNV/PAV/stable-copy enrichment results and an updated function to perform the same enrichment analysis but using N2 and HDR calls for each wild strain

--- scripts/hyperdivergent_regions/cnv_pav_HDR_enrichment.R
+++ scripts/hyperdivergent_regions/cnv_pav_HDR_enrichment.R
@@ -4,6 +4,7 @@ library(data.table)
 library(readr)
 library(rstatix)
 library(ggpubr)
+library(ape)
   
 # Calculating stats for every wild strain and appending to a master dataframe
 OG_enrichment <- function(ws_hdr_ogs, strains, og_matrix_relationships, single_copy_ogs) {
@@ -49,7 +50,7 @@ OG_enrichment <- function(ws_hdr_ogs, strains, og_matrix_relationships, single_c
     
     
     # outside of HDRs stats
-    nonhdr_og_relations <- og_matrix_relationships %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% filter(!(is.na(N2) & is.na(.data[[soi]]))) %>%
+    nonhdr_og_relations <- og_matrix_relationships %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% filter(!is.na(.data[[soi]])) %>%
       dplyr::mutate(
         relation = dplyr::case_when(
           is.na(N2) | is.na(.data[[soi]]) ~ "PAV",
@@ -57,7 +58,7 @@ OG_enrichment <- function(ws_hdr_ogs, strains, og_matrix_relationships, single_c
           TRUE                            ~ "CNV" )) %>%
       dplyr::count(relation, name = "count")
     
-    nonhdr_ogs <- og_matrix_relationships %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% filter(!(is.na(N2) & is.na(.data[[soi]])))
+    nonhdr_ogs <- og_matrix_relationships %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% filter(!is.na(.data[[soi]]))
     sg_ogs_nonHDR <- og_matrix_relationships %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% dplyr::filter(Orthogroup %in% single_copy_ogs) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
     
     
@@ -76,7 +77,7 @@ OG_enrichment <- function(ws_hdr_ogs, strains, og_matrix_relationships, single_c
 }
 
 # Preparing data:
-ws_hdr_ogs <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/ws_HDR_liftover/hdr_genes_OG_class.tsv")
+ws_hdr_ogs <- readr::read_tsv("../../processed_data/hdr_liftover/hdr_genes_OG_class.tsv")
 strains <- ws_hdr_ogs %>% dplyr::distinct(strain) %>% dplyr::pull()
 all_relations <- readr::read_tsv("../../processed_data/orthology/OG_relations_matrix_count.tsv")
 sc_ogs <- readr::read_tsv("../../processed_data/orthology/orthofinder/orthofinder_output/Orthogroups_SingleCopyOrthologues.txt", col_names = "OGs") %>%
@@ -133,7 +134,7 @@ all_plt <- ggplot(plot_df_norm, aes(x = stat, y = value, fill = region)) +
   geom_point(position = position_jitterdodge(jitter.width = 0.6, dodge.width = 0.75), size = 0.3) +
   scale_fill_manual(values = c("HDR" = "red", "non-HDR" = "blue")) +
   stat_pvalue_manual(wilcox_results, label = "p.adj.signif", x = "stat", y.position = "y.position", inherit.aes = FALSE, color = 'black', size = 4) +
-  # scale_y_log10() +
+  coord_cartesian(ylim = c(0,1)) +
   labs(y = "Proportion of orthogroups", fill = "Region") +
   theme_bw() +
   theme(
@@ -870,6 +871,210 @@ stats
 
 
 ############################################################################################################################################ 
+############################ Looking at CNV, PAV, and stable-copy enrichment for all wild strain HDRs with N2 ############################ 
+############################################################################################################################################
+N2_OG_enrichment <- function(all_N2_genes_OGs, strains, og_matrix_relationships, single_copy_ogs, hyperdivergent_regions) {
+  cnv_pav_results_df = as.data.frame(matrix(ncol = 11, nrow = 140))
+  names(cnv_pav_results_df) = c("strain_HDRs", "CNV_inHDR", "PAV_inHDR", "one_to_one_inHDR", "single_copy_OGs_inHDRs", "CNV_nonHDR", "PAV_nonHDR", "one_to_one_nonHDR", "single_copy_OGs_nonHDRs", "HDR_OG_count", "nonHDR_OG_count")
+  
+  # To prevent error-ing out because of no OG type being present
+  extract_count <- function(df, relation_type) {
+    val <- df %>% dplyr::filter(relation == relation_type) %>% dplyr::pull(count)
+    ifelse(length(val) == 0, 0, val)
+  }
+  
+  for (i in 1:length(strains)) {
+    soi <- strains[i]
+    print(paste0("On strain ", soi, " HDRs. ", i, "/140."))
+    
+    cnv_pav_results_df[i,1] = soi
+    
+    # HDRs stats
+    ## HDR calls for soi
+    hdrs <- hyperdivergent_regions %>% dplyr::filter(strain == soi)
+    
+    ## Look at overlap of N2 genes with soi HDRs
+    hdrs_dt <- data.table::as.data.table(hdrs)
+    n2_genes_hdr_classification <- data.table::as.data.table(all_N2_genes_OGs)
+    
+    # Setting the keys for foverlaps
+    data.table::setkey(hdrs_dt, chrom, start, end)
+    data.table::setkey(n2_genes_hdr_classification, chrom, start, end)
+    
+    n2_genes_hdrs <- foverlaps(
+      x = n2_genes_hdr_classification,
+      y = hdrs_dt,
+      type = "any",
+      nomatch = NA) 
+    
+    # Pulling N2 gene OGs in HDRs
+    n2_genes_hdr_OGs <- n2_genes_hdrs %>% dplyr::filter(!is.na(start)) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
+    
+    # Pulling N2 gene OGs NOT in HDRs
+    n2_genes_nonHDR_OGs <- n2_genes_hdrs %>% dplyr::filter(is.na(start)) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
+    
+    
+    # HDRs stats
+    cnv_pav_results_df[i,10] = length(n2_genes_hdr_OGs)
+    
+    hdr_og_relations <- og_matrix_relationships %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(Orthogroup %in% n2_genes_hdr_OGs) %>% filter(!is.na(N2)) %>%
+      dplyr::mutate(
+        relation = dplyr::case_when(
+          is.na(N2) | is.na(.data[[soi]]) ~ "PAV", # N2 will never be NA because N2 genes are being used to pull OGs
+          N2 == .data[[soi]]              ~ "one_to_one",
+          TRUE                            ~ "CNV" )) %>%
+      dplyr::count(relation, name = "count")
+    
+    sg_ogs <- og_matrix_relationships %>% dplyr::filter(Orthogroup %in% n2_genes_hdr_OGs) %>% dplyr::filter(Orthogroup %in% single_copy_ogs) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
+    
+    cnv_hdr <- hdr_og_relations %>% dplyr::filter(relation == "CNV") %>% dplyr::pull(count)
+    pav_hdr <- hdr_og_relations %>% dplyr::filter(relation == "PAV") %>% dplyr::pull(count)
+    one_to_one <- hdr_og_relations %>% dplyr::filter(relation == "one_to_one") %>% dplyr::pull(count)
+    
+    cnv_pav_results_df[i,2] = extract_count(hdr_og_relations, "CNV")
+    cnv_pav_results_df[i,3] = extract_count(hdr_og_relations, "PAV")
+    cnv_pav_results_df[i,4] = extract_count(hdr_og_relations, "one_to_one")
+    cnv_pav_results_df[i,5] = length(sg_ogs)
+    
+    
+    # outside of HDRs stats
+    nonhdr_og_relations <- og_matrix_relationships %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(Orthogroup %in% n2_genes_nonHDR_OGs) %>% filter(!is.na(N2)) %>%
+      dplyr::mutate(
+        relation = dplyr::case_when(
+          is.na(N2) | is.na(.data[[soi]]) ~ "PAV", # N2 will never be NA because N2 genes are being used to pull OGs
+          N2 == .data[[soi]]              ~ "one_to_one",
+          TRUE                            ~ "CNV" )) %>%
+      dplyr::count(relation, name = "count")
+    
+    nonhdr_ogs <- og_matrix_relationships %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(Orthogroup %in% n2_genes_nonHDR_OGs) %>% filter(!is.na(N2))
+    sg_ogs_nonHDR <- og_matrix_relationships %>% dplyr::filter(Orthogroup %in% n2_genes_nonHDR_OGs) %>% dplyr::filter(Orthogroup %in% single_copy_ogs) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
+    
+    
+    cnv_pav_results_df[i,11] = nrow(nonhdr_ogs)
+    
+    cnv_hdr <- nonhdr_og_relations %>% dplyr::filter(relation == "CNV") %>% dplyr::pull(count)
+    pav_hdr <- nonhdr_og_relations %>% dplyr::filter(relation == "PAV") %>% dplyr::pull(count)
+    one_to_one <- nonhdr_og_relations %>% dplyr::filter(relation == "one_to_one") %>% dplyr::pull(count)
+    
+    cnv_pav_results_df[i,6] = extract_count(nonhdr_og_relations, "CNV")
+    cnv_pav_results_df[i,7] = extract_count(nonhdr_og_relations, "PAV")
+    cnv_pav_results_df[i,8] = extract_count(nonhdr_og_relations, "one_to_one")
+    cnv_pav_results_df[i,9] = length(sg_ogs_nonHDR)
+  }
+  return(cnv_pav_results_df)
+}
+
+# Preparing data:
+## All wild strain HDR calls in relation to N2
+hdrs <- readr::read_tsv("../../data//20250625_c_elegans_divergent_regions_strain.bed", col_names = c("chrom","start","end","strain")) %>% dplyr::filter(strain %in% strains)
+## All N2 genes
+n2_genes_coords <- ape::read.gff("../../processed_data/genome_resources/annotation/c_elegans.PRJNA13758.WS283.csq.PCfeaturesOnly.longest.gff3") %>% dplyr::select(chrom = seqid,type,start,end,attributes) %>%
+  dplyr::filter(type == "mRNA") %>% tidyr::separate(attributes, into = c("gene","rest"), sep = ";") %>% dplyr::select(-rest) %>% dplyr::mutate(gene = gsub("ID=transcript:","",gene)) %>% dplyr::select(gene, chrom, start, end) %>%
+  dplyr::mutate(gene = sub("\\.[^.]*$", "", gene))
+## The orthogroup assignment of N2 genes and their pangengeom gene set contribution
+all_genes_OG <- readr::read_tsv("../../processed_data/orthology/all_genes_class_OGs.tsv") %>% dplyr::filter(strain == "N2") %>% dplyr::mutate(gene = gsub("transcript_","",gene)) %>%
+  dplyr::left_join(n2_genes_coords, by = "gene")
+
+all_relations <- readr::read_tsv("../../processed_data/orthology/OG_relations_matrix_count.tsv")
+sc_ogs <- readr::read_tsv("../../processed_data/orthology/orthofinder/orthofinder_output/Orthogroups_SingleCopyOrthologues.txt", col_names = "OGs") %>%
+  dplyr::pull(OGs)
+
+# Running N2 orthogroup enrichment
+n2_og_enrich_results <- N2_OG_enrichment(all_genes_OG, strains, all_relations, sc_ogs, hdrs)
+
+
+#### Calculating stats and adding to plot: 
+# Paired Wilcoxon signed-rank test
+plot_df_norm_n2 <- n2_og_enrich_results %>%
+  dplyr::rename(inHDR_OG_count = HDR_OG_count) %>%
+  dplyr::mutate(
+    dplyr::across(dplyr::matches("inHDR"), ~ .x / inHDR_OG_count),
+    dplyr::across(dplyr::matches("nonHDR"), ~ .x / nonHDR_OG_count)) %>%
+  tidyr::pivot_longer(
+    cols = -strain_HDRs,
+    names_to = "metric",
+    values_to = "value") %>%
+  dplyr::mutate(region = case_when(
+    stringr::str_detect(metric, "nonHDR") ~ "non-HDR",
+    stringr::str_detect(metric, "HDR") ~ "HDR",
+    TRUE ~ NA)) %>%
+  dplyr::filter(!is.na(region)) %>%
+  dplyr::mutate(region = factor(region, levels = c("HDR", "non-HDR")),
+                metric = ifelse(metric == "inHDR_OG_count", "OG_count",
+                                ifelse(metric == "nonHDR_OG_count", "OG_count", metric)),
+                stat = metric %>%
+                  stringr::str_remove("_inHDRs?$") %>%
+                  stringr::str_remove("_nonHDRs?$"),
+                stat = ifelse(stat == "one_to_one","n-to-n",
+                              ifelse(stat == "single_copy_OGs", "single-copy OGs",
+                                     ifelse(stat == "OG_count", "OGs", stat))),
+                stat = factor(stat, levels = c("CNV","PAV","n-to-n","single-copy OGs","OGs"))) %>%
+  dplyr::filter(stat != "OGs") %>%
+  dplyr::mutate(value = ifelse(value == 'NaN', 0, value)) # to account for potential 0 dividided by 0 instances
+
+wilcox_result_n2 <- plot_df_norm_n2  %>%
+  dplyr::group_by(stat) %>%
+  wilcox_test(value ~ region, paired = TRUE) %>%
+  adjust_pvalue(method = "BH") %>% # Benjamini-Hochberg correction
+  add_significance()
+
+y_pos_n2 <- plot_df_norm_n2 %>%
+  dplyr::group_by(stat) %>%
+  dplyr::summarise(y.position = max(value, na.rm = TRUE) * 1.03, .groups = "drop")
+
+wilcox_results_n2 <- wilcox_result_n2 %>%
+  dplyr::left_join(y_pos_n2, by = "stat")
+
+n2_all_plt <- ggplot(plot_df_norm_n2, aes(x = stat, y = value, fill = region)) +
+  geom_boxplot(width = 0.7, position = position_dodge(width = 0.75), outlier.shape = NA, alpha = 0.5) +
+  geom_point(position = position_jitterdodge(jitter.width = 0.6, dodge.width = 0.75), size = 0.3) +
+  scale_fill_manual(values = c("HDR" = "red", "non-HDR" = "blue")) +
+  stat_pvalue_manual(wilcox_results_n2, label = "p.adj.signif", x = "stat", y.position = "y.position", inherit.aes = FALSE, color = 'black', size = 4) +
+  # scale_y_log10() +
+  labs(y = "Proportion of orthogroups", fill = "Region") +
+  theme_bw() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(size = 11, color = 'black'),
+    panel.grid.major.x = element_blank(),
+    legend.box.background = element_rect(color = "black", linewidth = 1),
+    legend.position  = 'inside',
+    panel.border = element_rect(color = 'black', fill =NA),
+    legend.position.inside = c(0.12, 0.85),
+    legend.title = element_blank(),
+    legend.key.size = unit(1.5, "cm"),
+    legend.text = element_text(size = 10, color = 'black'),
+    axis.text.y = element_text(size = 10, color = 'black'),
+    axis.title.y = element_text(size = 11, color = 'black')
+  )  +
+  scale_x_discrete(labels = c(
+    "CNV" = expression("CNV"),
+    "PAV" = expression("PAV"),
+    "n-to-n" = expression(italic(n) * "-to-" * italic(n)),
+    "single-copy OGs" = expression("single-copy orthogroups")))
+n2_all_plt
+
+# Save the plot
+# ggsave("../../figures/supplementary/cnv_pav_N2_allGenes.png", width = 7.5, height = 5.5, dpi = 600)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################################################################################################################################ 
 ############################ Single-copy ortholog proportions in HDRs ############################ 
 ############################################################################################################################################
 ### Which gene family has the most single-copy orthologs in the HDRs in relation to all single-copy orthologs genome-wide for that gene family????
@@ -1142,6 +1347,4 @@ sc_ogs_cyt_freq <- sc_ogs_cyt_freq %>% dplyr::left_join(OG_freq, by = "OGs") %>%
 ################# Will this table be written??
 
 # write.table(sc_ogs_cyt_freq, "/vast/eande106/projects/Lance/THESIS_WORK/assemblies/orthology/elegans/orthofinder/cytoP450_sc_OGs.tsv", col.names = T, row.names = F, quote= F, sep =)
-
-
 
